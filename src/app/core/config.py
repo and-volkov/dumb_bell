c1435fe6b077b3977dfc83e25d93463461f12e5d
Andrey Volkov <andrey.dreamwall@gmail.com>
2024-02-04 11:25:16 +0400

    init

---
+++ src/app/core/config.py
@@ -0,0 +1,75 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+from typing import Any, Dict, Optional
+
+from dotenv import load_dotenv
+from pydantic import AnyHttpUrl, PostgresDsn, field_validator
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+
+    PROJECT_NAME: str
+
+    SERVER_NAME: str
+    SERVER_HOST: AnyHttpUrl
+
+    BACKEND_CORS_ORIGINS: list[str] = ["*"]
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    SQLALCHEMY_DATABASE_URI: str
+
+    LOG_FILE_NAME: str = "logs.log"
+    LOG_FILE_PATH: str = "logs"
+    LOG_MESSAGE_FORMAT: str = "%(asctime)s %(levelname)s: %(message)s"
+    LOG_DATE_FORMAT: str = "%Y-%m-%d %H:%M:%S"
+
+    @field_validator("SQLALCHEMY_DATABASE_URI")  # type: ignore
+    def assemble_db_connection(
+        cls, v: Optional[str], values: Dict[str, Any]
+    ) -> PostgresDsn | str:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=(
+                f"{values.get("POSTGRES_HOST") or "127.0.0.1"}"
+                f":{values.get("POSTGRES_PORT") or 5432}"
+            ),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()  # type: ignore
+
+
+log_level = logging.INFO
+log_stream_handler = logging.StreamHandler()
+log_file_handler = RotatingFileHandler(
+    filename=settings.LOG_FILE_PATH + os.pathsep + settings.LOG_FILE_NAME,
+    mode="a",
+    maxBytes=5 * 1024 * 1024,
+    backupCount=1,
+    encoding=None,
+)
+
+logging.basicConfig(
+    encoding="utf-8",
+    level=log_level,
+    format=settings.LOG_MESSAGE_FORMAT,
+    handlers=[log_file_handler, log_stream_handler],
+    datefmt=settings.LOG_DATE_FORMAT,
+)
